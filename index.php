4ab0e24c5eee8d421d5004112c0bc459dc3fcc92
nero3327 <43396258+nero3327@users.noreply.github.com>
2019-12-05 09:46:20 +0800

    add <br> from mobile to see better

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <html>
 <body>
 <form action="get.php" method="POST">
-URL:  <input type="text" name="url" value="HTTP://">
+URL:  <input type="text" name="url" value="url">
+<br>
 PAGE  <input type="text" name="pageNumber" value="15">
+<br>
 keywords <input type="text" name="keyWords" value="下一章">
 <br>
 enCode <input type="text" name="enCode" value="GBK">
